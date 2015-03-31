c6aaeece55c2c566c448b5efd94c3aededd778f1
Sam Holmes <samholmes1337@gmail.com>
2015-03-31 22:30:11 +0100

    Added POST support to email

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.render('index.jade');
 });
+
+app.post('/email', function(req, res){
+    if (typeof req.body.email !== 'undefined'){
+        var info = req.body.email + ",";
+        fs.appendFile('emails.csv', info, function(error){
+            if (error){
+                console.log(error);
+            };
+        });
+        res.send(req.body.email);
+    }
+})
 
 app.listen(app.get('port'), function () {
     console.log("Node app is running at localhost:" + app.get('port'));
--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -1,7 +1,15 @@
 var app = angular.module('app', []);
 
-app.controller('MainCtrl', function ($scope) {
-    $scope.addEmail = function (signup) {
-        console.log(signup.email);
-    }
-});
+app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
+    $scope.addEmail = function() {
+        $http.post('/email', $scope.signup).success(function(){
+
+            // This gets called when the users email
+            // has been successfully added to the list
+            // of emails. Angular should automatically
+            // replace the email field with a
+            // confirmation to let the user know that.
+
+        });
+    };
+}]);
