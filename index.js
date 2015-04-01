348afef1e696c22b4f474b2b0332d61fca278d72
Henry Kaufman <hkaufman19@gmail.com>
2015-04-01 16:23:27 -0600

    Started to work on the dashboard, and we now have a basic idea on how the dashboard is going to look.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
     res.render('index.jade');
 });
 
+app.get('/dashboard', function (req, res) {
+    res.render('dashboard.jade');
+});
+
 app.listen(app.get('port'), function () {
     console.log("Node app is running at localhost:" + app.get('port'));
 });
